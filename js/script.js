ab2cc6a7c4b62fd05f94f3348ee555d1863b31dd
Afonya <afon4ik.322@mail.ru>
2022-08-14 21:22:07 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// alert('hello');
+// const result = confirm("Are you here?");
+// const answer = prompt('Are you 18?',"");
+
+
+// let number =5;
+// const leftBorderWidth =1;
+
+// number =10;
+// console.log(number);
+
+
+// const obj = {
+//     a : 50
+// };
+
+// obj.a = 10;
+
+
+// var name = 'Ivan';
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Как ваше отчество', '');
